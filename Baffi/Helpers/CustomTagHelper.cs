2fd988c97b26d9e7300b604b236934821c360b0a
Viktor Nilsson <viktor.nilsson@nelly.com>
2017-11-13 10:09:08 +0100

    Removed unused nuget.

--- Baffi/Helpers/CustomTagHelper.cs
+++ Baffi/Helpers/CustomTagHelper.cs
@@ -1,25 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using Microsoft.Extensions.DependencyModel;
 
 namespace Baffi.Helpers
 {
     internal static class CustomTagHelper
     {
-        internal static List<Type> GetAllCustomTagTypes()
-        {
-            var assemblyNames = DependencyContext.Default.GetDefaultAssemblyNames();
-            var type = typeof(ICustomTag);
-
-            var allTypes = assemblyNames.Select(Assembly.Load)
-                .SelectMany(t => t.GetTypes())
-                .Where(p => p.GetTypeInfo().ImplementedInterfaces.Contains(type)).ToList();
-
-            return allTypes;
-        }
-
         internal static string[] GetTagParameters(string customTag)
         {
             char[] splitChars = { '=', '-', '&' };
